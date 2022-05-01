c50d82c4db4c7ca892184d71000301f0b739946f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-01 16:08:06 +0300

    add completed Kata "Basic Mathematical Operations"

---
+++ script17.js
@@ -0,0 +1,14 @@
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+    }
+}
+
+console.log(basicOp('-', 2, 3));
