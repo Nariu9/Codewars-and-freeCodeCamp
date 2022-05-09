2966b417829886b520479e842ce7be81fdd1d920
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-09 19:18:20 +0300

    add completed Kata "Your order, please"

---
+++ script51.js
@@ -0,0 +1,7 @@
+function order(words){
+    return words.split(' ').sort((a, b) => {
+        return a.match(/\d/) - b.match(/\d/);
+    }).join(' ');
+}
+
+console.log(order("is2 Thi1s T4est 3a"));
