194540070829ef374e8d4b051d235473ce8c02cf
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-03 11:12:43 +0200

    add completed Kata "Thinking & Testing: A and B?"

---
+++ script139.js
@@ -0,0 +1,10 @@
+function testit(a,b){
+   return a | b
+}
+
+console.log(testit(0,1))
+console.log(testit(1,2))
+console.log(testit(10,20))
+console.log(testit(1,1))
+console.log(testit(1,3))
+console.log(testit(2,2))
